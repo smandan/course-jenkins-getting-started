91a6728125d7e108818d5a93d4d467f08bad0c71
Suresh Mandan <mandas1@nationwide.com>
2020-10-15 18:28:54 -0400

    commiting changes to pipeline files

--- pipeline/spc/pipeline.groovy
+++ pipeline/spc/pipeline.groovy
@@ -1,21 +1,39 @@
-// Powered by Infostretch 
+pipeline {
+    agent any
 
-timestamps {
+//  tools {
+//         // Install the Maven version configured as "M3" and add it to the path.
+//         maven "M3"
+//  }
 
-node () {
+    stages {
+        stage('Checkout') {
+            steps {
+            // Get some code from a GitHub repository
+            git branch: 'main', url: 'https://github.com/smandan/jgsu-spring-petclinic.git'
 
-	stage ('spring-petclinic - Checkout') {
- 	 checkout([$class: 'GitSCM', branches: [[name: '*/main']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '', url: 'https://github.com/smandan/jgsu-spring-petclinic.git']]]) 
-	}
-	stage ('spring-petclinic - Build') {
- 			// Shell build step
-sh """ 
-mvn spring-javaformat:apply clean package
-# ./mvnw spring-javaformat:apply clean package 
- """
-		archiveArtifacts allowEmptyArchive: false, artifacts: 'target/*.jar', caseSensitive: true, defaultExcludes: true, fingerprint: false, onlyIfSuccessful: false
-		// JUnit Results
-		junit 'target/surefire-reports/*.xml' 
-	}
-}
+            }
+        }
+
+        stage('Build') {
+            steps {
+                // Run Maven on a Unix agent.
+                // sh "mvn -Dmaven.test.failure.ignore=true clean package"
+                // sh "mvn spring-javaformat:apply clean package"
+                sh "mvn spring-javaformat:apply -Dmaven.test.failure.ignore=true clean package"
+
+                // To run Maven on a Windows agent, use
+                // bat "mvn spring-javaformat:apply  -Dmaven.test.failure.ignore=true clean package"
+            }
+            post {
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
+                always {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+
+    }
 }
