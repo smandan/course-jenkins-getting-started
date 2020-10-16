9d9213e582a7499eb113c77b7824b32be962c447
Suresh Mandan <mandas1@nationwide.com>
2020-10-16 19:40:27 -0400

    modified the pipeline script

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -31,6 +31,14 @@ pipeline {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
+//              }
+//              changed {
+                    emailext attachLog: true,
+                    body: 'Please go to ${BUILD_URL} and verify the build',
+                    compressLog: true,
+                    recipientProviders: [upstreamDevelopers(), requestor()],
+                    subject: "Job ${JOB_NAME} (build ${BUILD_NUMBER}) ${currentBuild.result}",
+                    to: 'mandas1@nationwide.com s.gadepalli@nationwide.com'
                 }
             }
         }
