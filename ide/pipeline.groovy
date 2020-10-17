3fd993db63d7cc8a37729dbe129efe03e567c8db
Suresh Mandan <mandas1@nationwide.com>
2020-10-16 21:26:34 -0400

    added a change section to the post clause

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -17,22 +17,24 @@ pipeline {
 
         stage('Build') {
             steps {
-                // Run Maven on a Unix agent.
-                // sh "mvn -Dmaven.test.failure.ignore=true clean package"
-                // sh "mvn spring-javaformat:apply clean package"
+//              Run Maven on a Unix agent.
+//              sh "mvn -Dmaven.test.failure.ignore=true clean package"
+//              sh "mvn spring-javaformat:apply clean package"
                 sh "mvn spring-javaformat:apply -Dmaven.test.failure.ignore=true clean package"
+//              sh "false" // true
+//              sh "true"  // false
 
-                // To run Maven on a Windows agent, use
-                // bat "mvn spring-javaformat:apply  -Dmaven.test.failure.ignore=true clean package"
+//              To run Maven on a Windows agent, use
+//              bat "mvn spring-javaformat:apply  -Dmaven.test.failure.ignore=true clean package"
             }
             post {
-                // If Maven was able to run the tests, even if some of the test
-                // failed, record the test results and archive the jar file.
+//              If Maven was able to run the tests, even if some of the test
+//              failed, record the test results and archive the jar file.
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
-//              }
-//              changed {
+                }
+                changed {
                     emailext attachLog: true,
                     body: 'Please go to ${BUILD_URL} and verify the build',
                     compressLog: true,
